86f674403be3c960ad0a09f84b45d655250b4d20
ericfue <107294764+ericfue@users.noreply.github.com>
2023-10-15 01:40:10 -0400

    Fix various bugs.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,21 @@
+let playerWins = 0;
+let computerWins = 0;
+
 game();
 
-function playRound(playerChoice, computerChoice) {
-    let playlen = playerChoice.length;
-    let complen = computerChoice.length;
-    if (playlen - complen === -4) {
-        return youWin(playerChoice, computerChoice);
-    } else if (playlen - complen === 4 || playlen < complen) {
-        return youLose(playerChoice, computerChoice);
-    } else if (playlen === complen) {
-        return "Tie!"
-    } else {
-        return youWin(playerChoice, computerChoice)
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getPlayerChoice(), getComputerChoice());
+        console.log("Round " + (i + 1) + " complete.");
+        console.log("Player Score: " + playerWins)
+        console.log("Computer Score: " + computerWins);
     }
-}
-
-function youLose(playerChoice, computerChoice) {
-    return "You lose! " + computerChoice + " beats " + playerChoice + "!";
-}
-
-function youWin(playerChoice, computerChoice) {
-    return "You win! " + playerChoice + " beats " + computerChoice + "!";
-}
-
-function getComputerChoice() {
-    let compChoice = (Math.floor(Math.random() * 300)) % 3;
-    switch(compChoice) {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
+    if (playerWins > computerWins) {
+        console.log("You win the match!");
+    } else if (playerWins === computerWins {
+        console.log("You tie the match!")
+    } else {
+        console.log("You lose the match!");
     }
 }
 
@@ -46,33 +31,46 @@ function capitalize(str) {
     return cap.concat(end);
 }
 
-function game() {
-    let compScore = 0;
-    let playScore = 0;
-    let playerChoice = null;
-    let computerChoice = null;
-    let result = null;
-    for (let i = 0; i < 5; i++) {
-        playerChoice = getPlayerChoice();
-        computerChoice = getComputerChoice();
-        console.log("Round: " + (i + 1));
-        result = playRound(playerChoice, computerChoice);
-        if (result.includes("lose")) {
-            compScore++;
-        } else if (result.includes("tie")) {
-            playScore += 0;
-            compScore += 0;
-        } else {
-            playScore++;
-        }
-        console.log(playerChoice);
-        console.log(computerChoice);
-        console.log("Playscore: " + playScore)
-        console.log("Compscore: " + compScore);
+function getComputerChoice() {
+    let compChoice = (Math.floor(Math.random() * 300)) % 3;
+    switch(compChoice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
     }
-    if (playScore > compScore) {
-        console.log("You win the match!");
+}
+
+function playRound(playerChoice, computerChoice) {
+    console.log("Player: " + playerChoice);
+    console.log("Computer: " + computerChoice);
+    let playlen = playerChoice.length;
+    let complen = computerChoice.length;
+    if (playlen - complen === -4) {
+        youWin(playerChoice, computerChoice);
+    } else if (playlen - complen === 4 || playlen < complen) {
+        youLose(playerChoice, computerChoice);
+    } else if (playlen === complen) {
+        youTie(playerChoice, computerChoice);
     } else {
-        console.log("You lose the match!");
+        youWin(playerChoice, computerChoice)
     }
+}
+
+function youLose(playerChoice, computerChoice) {
+    computerWins++;
+    console.log("You lose! " + computerChoice + " beats " + playerChoice + "!");
+}
+
+function youTie(playerChoice, computerChoice) {
+    computerWins += 0;
+    playerWins += 0;
+    console.log(playerChoice + " ties with " + computerChoice + ".");
+}
+
+function youWin(playerChoice, computerChoice) {
+    playerWins++;
+    console.log("You win! " + playerChoice + " beats " + computerChoice + "!");
 }
